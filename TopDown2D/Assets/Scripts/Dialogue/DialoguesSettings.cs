d3f2bc93cdec322671af70b5f8914cff1495ac35
SoulsDevStudio <101211878+SoulsDevStudio@users.noreply.github.com>
2022-04-14 00:12:05 -0300

    Create NPC

---
+++ TopDown2D/Assets/Scripts/Dialogue/DialoguesSettings.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "New Dialogue/Dialogue")]
+
+public class DialoguesSettings : ScriptableObject
+{
+
+    [Header("Settings")]
+    public GameObject actor;
+
+    [Header("Dialogue")]
+    public Sprite speakrSprite;
+    public string sentence;
+
+    public List<Sentences> dialogues = new List<Sentences>();
+
+}
+
+[System.Serializable]
+public class Sentences
+{
+    public string actorName;
+    public Sprite profile;
+    public Languages sentence;
+}
+
+[System.Serializable]
+public class Languages
+{
+    public string portuguese;
+    public string english;
+    public string spanish;
+}
