ef4962552daab0c53c984d42d6a3f5531d4bab9d
SoulsDevStudio <101211878+SoulsDevStudio@users.noreply.github.com>
2022-04-19 17:24:59 -0300

    Next Sentence Dialogue and Language Dialogue

--- TopDown2D/Assets/Scripts/NPC/NpcDialogue.cs
+++ TopDown2D/Assets/Scripts/NPC/NpcDialogue.cs
@@ -31,7 +31,18 @@ public class NpcDialogue : MonoBehaviour
     {
         for(int i = 0; i < dialogue.dialogues.Count; i++)
         {
-            sentences.Add(dialogue.dialogues[i].sentence.portuguese);
+            switch(DialogueControl.instance.language){
+                case DialogueControl.idiom.pt:
+                    sentences.Add(dialogue.dialogues[i].sentence.portuguese);
+                    break;
+                case DialogueControl.idiom.eng:
+                    sentences.Add(dialogue.dialogues[i].sentence.english);
+                    break;
+                case DialogueControl.idiom.spa:
+                    sentences.Add(dialogue.dialogues[i].sentence.spanish);
+                    break;
+            }
+            
         }
     }
 
@@ -51,7 +62,6 @@ public class NpcDialogue : MonoBehaviour
         else
         {
             playerHit = false;
-            DialogueControl.instance.dialogueObj.SetActive(false);
         }
     }
 
