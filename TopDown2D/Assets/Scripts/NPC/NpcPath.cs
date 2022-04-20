ee7bbff2feebc97a89db81e8059d14af859d4d25
SoulsDevStudio <101211878+SoulsDevStudio@users.noreply.github.com>
2022-04-20 15:18:27 -0300

    NPC Animation Walking and Script Path Npc

---
+++ TopDown2D/Assets/Scripts/NPC/NpcPath.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcPath : MonoBehaviour
+{
+
+    public float speed;
+
+    private int index;
+
+    public List<Transform> paths = new List<Transform>();
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, paths[index].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, paths[index].position) < 0.1f)
+        {
+            if(index < paths.Count - 1)
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+        }
+
+        Vector2 direction = paths[index].position - transform.position;
+
+        if(direction.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if(direction.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+}
