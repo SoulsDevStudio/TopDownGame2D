427b3134c86cf869ab36a69a3e63e449967733d6
SoulsDevStudio <101211878+SoulsDevStudio@users.noreply.github.com>
2022-04-28 15:37:03 -0300

    Crafting - Drop

--- TopDown2D/Assets/Scripts/Craft/Tree.cs
+++ TopDown2D/Assets/Scripts/Craft/Tree.cs
@@ -6,6 +6,9 @@ public class Tree : MonoBehaviour
 {
     [SerializeField] private float treeHealth;
     [SerializeField] private Animator anim;
+
+    [SerializeField] private GameObject woodPrefab;
+    [SerializeField] private int totalDrop;
     
     public void OnHit() 
     {
@@ -15,6 +18,12 @@ public class Tree : MonoBehaviour
 
         if(treeHealth <= 0)
         {
+            totalDrop = Random.Range(1,5); 
+            for (int i = 0; i < totalDrop; i++)
+            {
+                Instantiate(woodPrefab, transform.position + new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0f), transform.rotation);
+            }
+            
             anim.SetTrigger("cut");
         }
     }
---
+++ TopDown2D/Assets/Scripts/Player/PlayerInventary.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventary : MonoBehaviour
+{
+    [SerializeField] private int _totalWood;
+
+    public int totalWood { get => _totalWood; set => _totalWood = value; }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
