3a48866af9da046273b9594416c48959a834033e
SoulsDevStudio <101211878+SoulsDevStudio@users.noreply.github.com>
2022-05-03 13:57:08 -0300

    Planting - Watering and Harvesting

--- TopDown2D/Assets/Scripts/Farm/SlotFarm.cs
+++ TopDown2D/Assets/Scripts/Farm/SlotFarm.cs
@@ -4,18 +4,53 @@ using UnityEngine;
 
 public class SlotFarm : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] private Sprite hole;
     [SerializeField] private Sprite carrot;
-
     [SerializeField] private SpriteRenderer spriteRender;
 
+    [Header("Settings")]
     [SerializeField] private int digAmount;
+    [SerializeField] private float waterAmount;
+    [SerializeField] private bool detecting;
 
     private int initialDigAmount;
 
+    private float currentWater;
+
+    private bool dugHole;
+
+    PlayerInventary playerInventary;
+
     private void Start()
     {
+        playerInventary = FindObjectOfType<PlayerInventary>();
+
         initialDigAmount = digAmount;
+    }
+
+    private void Update()
+    {
+        if (dugHole)
+        {
+            if (detecting)
+            {
+                currentWater += 0.01f;
+            }
+
+            if (currentWater >= waterAmount)
+            {
+                spriteRender.sprite = carrot;
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    playerInventary.TotalCarrot++;
+                    spriteRender.sprite = hole;
+                    currentWater = 0f;
+                }
+            }
+        }
+        
     }
 
     public void OnHit()
@@ -25,12 +60,9 @@ public class SlotFarm : MonoBehaviour
         if(digAmount <= 0)
         {
             spriteRender.sprite = hole;
+            dugHole = true;
         }
 
-        //if (digAmount <= 0)
-        //{
-        //    spriteRender.sprite = carrot;
-        //}
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -39,6 +71,19 @@ public class SlotFarm : MonoBehaviour
         {
             OnHit();
 
+        }
+
+        if (collision.CompareTag("Water"))
+        {
+            detecting = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Water"))
+        {
+            detecting = false;
         }
     }
 
--- TopDown2D/Assets/Scripts/Player/PlayerInventary.cs
+++ TopDown2D/Assets/Scripts/Player/PlayerInventary.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerInventary : MonoBehaviour
 {
     [SerializeField] private int _totalWood;
+    [SerializeField] private int _totalCarrot;
     [SerializeField] private float _currentWater;
     [SerializeField] private float _waterLimit;
 
     public int totalWood { get => _totalWood; set => _totalWood = value; }
     public float currentWater { get => _currentWater; set => _currentWater = value; }
     public float waterLimit { get => _waterLimit; set => _waterLimit = value; }
+    public int TotalCarrot { get => _totalCarrot; set => _totalCarrot = value; }
 
     public void WaterLimit(float water)
     {
