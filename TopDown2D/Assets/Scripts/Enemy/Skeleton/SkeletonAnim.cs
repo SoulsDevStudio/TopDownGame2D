65813aa0b1a0b7e1741abf353ace1aa341ab1454
SoulsDevStudio <101211878+SoulsDevStudio@users.noreply.github.com>
2022-05-11 14:42:52 -0300

    Combat - Enemy Attack

--- TopDown2D/Assets/Scripts/Enemy/Skeleton/SkeletonAnim.cs
+++ TopDown2D/Assets/Scripts/Enemy/Skeleton/SkeletonAnim.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SkeletonAnim : MonoBehaviour
 {
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask playerLayer;
+
     private Animator anim;
 
     private void Awake()
@@ -14,5 +18,24 @@ public class SkeletonAnim : MonoBehaviour
     public void PlayAnim(int value)
     {
         anim.SetInteger("transition", value);
+    }
+
+    public void Attack()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position,radius,playerLayer);
+
+        if(hit != null)
+        {
+            Debug.Log("Atacou");
+        }
+        else
+        {
+            
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, radius);
     }
 }
